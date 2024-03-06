dfddb09b06ee3e425532260b911a5f47ee47f764
I03Ishika <125553810+I03Ishika@users.noreply.github.com>
2024-03-07 00:12:34 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+import Tempapp from './components/Tempapp';
+
+const App = () => {
+    return(
+        <>
+            <Tempapp />
+        </>
+    );
+};
+
+export default App;
